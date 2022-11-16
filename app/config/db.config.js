b5430ec32a12913ba6b1beb8d266fe0195b606c1
Agustin Simon <agustinsimon@MBP-de-Agustin.fibertel.com.ar>
2022-11-16 19:16:58 -0300

    conexion mongo db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -30,4 +30,4 @@ module.exports=()=>{
 //Paso 1: https://www.youtube.com/watch?v=iN9wogzhjMU&ab_channel=LeiferMendez
 //Paso 2:https://www.youtube.com/watch?v=6FoKDYDJ4V8&ab_channel=LeiferMendez
 
-//mongodb+srv://agustinsimon07:Agustin18Simon13@geolocalizacionbd.sgnnuya.mongodb.net/test
+//mongodb+srv://agustinsimon07:Agustin18Simon13@geolocalizacionbd.sgnnuya.mongodb.net/geolocalizacionDatabase
