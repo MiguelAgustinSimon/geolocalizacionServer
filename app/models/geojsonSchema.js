aeeca4d7841da32dbc363f1c3092098328f1c912
Agustin Simon <agustinsimon@MBP-de-Agustin.fibertel.com.ar>
2023-03-13 13:49:22 -0300

    refresh

--- app/models/geojsonSchema.js
+++ app/models/geojsonSchema.js
@@ -24,31 +24,5 @@ const schema = {
     }
   };
 
-
-  // const schema = {
-  //   type: 'object',
-  //   additionalProperties:true, 
-  //   required:['geometry'],
-  //   properties: {
-  //     //geometry: { "type": "number", "multipleOf": 0.000000000000001 }
-  //     geometry: { "type": "object" ,"required": ["coordinates"],
-  //       properties: {
-  //         coordinates: { type: "array",
-  //           items: {type: "array",
-  //             items: {
-  //               "type": "string",
-  //               "pattern": '^\-?[0-9][0-9][.]\\d{2,}[,]\\s?\-?[0-9][0-9][.]\\d{2,}$'
-  //             },
-  //             errorMessage:{
-  //               type:'Coordinates debe ser array numérico'
-  //             }
-  //           }
-  //         }
-  //       },
-  //     }
-  //   }
-  // };
-
-
   
 module.exports = ajvInstance.compile(schema);
